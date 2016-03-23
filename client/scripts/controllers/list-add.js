580e973b92d4f1b397c826540065f4f176aefd65
Shahar Lahav <lahav.shahar@gmail.com>
2016-03-23 16:53:39 +0200

    material add list

--- client/scripts/controllers/list-add.js
+++ client/scripts/controllers/list-add.js
@@ -3,16 +3,16 @@ angular
 	.controller('ListAddCtrl', Ctrl);
 
 function Ctrl($scope, $location) {
-//$scope.nameName = "Enter here";
+	//$scope.nameName = "Enter here";
 
-	$scope.CreateList = function(nameName,description) { 		
+	$scope.CreateList = function(nameName, description) {
 		var list = {
 			name: nameName,
 			description: description
 		};
+		$scope.nameName = '';
+		$scope.description = '';
 		Lists.insert(list);
-		 $scope.nameName = '';
-		 $scope.description = '';
-		 $location.path("#/tabs/Lists");		
+		$location.path("#/tabs/Lists");
 	}
 }
